f06fbf70ac930e870968a5054dfd50e0697c437f
Jabneel <zjabneel@gmail.com>
2021-07-01 10:57:37 -0400

    adding port for heroku api

--- server.js
+++ server.js
@@ -35,8 +35,8 @@ app.get('/profile/:id', (req,res) => { profile.handleProfileGet(req, res, db)} )
 app.put('/image', (req,res) => { image.handleImage(req, res, db)}) 
 app.post('/imageUrl', (req,res) => { image.handleApiCall(req, res)} )
 
-app.listen(3000, ()=> {
-	console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, ()=> { //heroku needs to run on its own port by process.env.PORT
+	console.log(`app is running on port ${process.env.PORT}`);
 })
 
 /*
